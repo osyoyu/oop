891ccd31c92f6cddf93f4d52b74a913ea0ac172a
osyoyu <osyoyu@osyoyu.com>
2015-06-28 22:23:47 +0900

    Include config.php only if it exists

--- oop.php
+++ oop.php
@@ -1,8 +1,11 @@
 <?php
 
-define('APP_PATH', realpath("../"));
+define('APP_PATH', realpath(dirname(__FILE__). "/../"));
 
 require_once(dirname(__FILE__) . "/lib/model.php");
 require_once(dirname(__FILE__) . "/lib/render.php");
 
-require_once(dirname(__FILE__) . "/app/config.php");
+if (file_exists(APP_PATH . "/app/config.php"))
+{
+  require_once(APP_PATH . "/app/config.php");
+}
