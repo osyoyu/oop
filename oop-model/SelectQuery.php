e1550cd35f8f0ba629af98f83009fabac9ee08aa
osyoyu <osyoyu@osyoyu.com>
2015-07-07 17:42:54 +0900

    Don't start a new transaction in SelectQuery

--- oop-model/SelectQuery.php
+++ oop-model/SelectQuery.php
@@ -51,11 +51,9 @@ class SelectQuery
   {
     global $db;
 
-    $db->beginTransaction();
     $statement = $db->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
     $statement->execute($placeholders);
     $results = $statement->fetchAll(PDO::FETCH_CLASS, $this->table);
-    $db->commit();
 
     return $results;
   }
