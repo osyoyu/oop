e540cff0b8d90674d170e3e0e7d73f5b1abfc8b1
osyoyu <osyoyu@osyoyu.com>
2015-07-07 17:01:47 +0900

    Implement BaseModel#create()

--- oop-model/BaseModel.php
+++ oop-model/BaseModel.php
@@ -22,9 +22,55 @@ abstract class BaseModel {
     return self::query();
   }
 
-
-  function __construct()
+  /**
+   * SQL INSERT
+   *
+   * return BaseModel
+   */
+  static function create($attrs)
   {
+    global $db;
+
+    if (!is_assoc($attrs)) { throw new InvalidArgumentException(); }
+
+    $fields = [];
+    $values = [];
+    $placeholders = [];
+
+    foreach ($attrs as $key => $value)
+    {
+      $fields[] = $key;
+      $hashed_key = ":" . md5("insert-" . $key);
+      $values[] = $hashed_key;
+      $placeholders[$hashed_key] = $value;
+    }
+
+    $sql = "INSERT INTO " . get_called_class() . " (" . implode(", ", $fields) . ") VALUES (" . implode(", ", $values) . ");";
+
+    $db->beginTransaction();
+    $statement = $db->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+    $statement->execute($placeholders);
+
+    $result = self::query()->where(["id" => $db->lastInsertID()])->call();
+    $db->commit();
+
+    return $result;
+  }
+
+
+  function __construct($saved = true)
+  {
+    $this->saved = $saved;
+  }
+
+  /**
+   * SQL UPDATE
+   */
+  function save()
+  {
+    global $db;
+
+    $sql = "";
   }
 
 }
