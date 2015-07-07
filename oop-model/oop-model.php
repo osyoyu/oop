866941415491d988b8123860c2f68616c3d6ec76
osyoyu <osyoyu@osyoyu.com>
2015-07-07 13:15:13 +0900

    Add UpdateQuery

--- oop-model/oop-model.php
+++ oop-model/oop-model.php
@@ -2,7 +2,8 @@
 
 require_once(dirname(__FILE__) . "/db.php");
 require_once(dirname(__FILE__) . "/BaseModel.php");
-require_once(dirname(__FILE__) . "/Query.php");
+require_once(dirname(__FILE__) . "/SelectQuery.php");
+require_once(dirname(__FILE__) . "/UpdateQuery.php");
 
 function load_model($model_name)
 {
